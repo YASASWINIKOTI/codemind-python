882b251d4039910e5441f34792e2f03e645027cc
YasaswiniKoti <119840576+YASASWINIKOTI@users.noreply.github.com>
2023-05-16 20:06:41 +0530

    def sum(a):     s=0     while(a>0):         r=a%10         s=s+r         a=a//10     return s n=int(input()) sqr=n**2 if(sum(sqr)==n):     print("Neon Number") else:     print("Not Neon Number")

---
+++ Neon_Number.py
@@ -0,0 +1,13 @@
+def sum(a):
+    s=0
+    while(a>0):
+        r=a%10
+        s=s+r
+        a=a//10
+    return s
+n=int(input())
+sqr=n**2
+if(sum(sqr)==n):
+    print("Neon Number")
+else:
+    print("Not Neon Number")
