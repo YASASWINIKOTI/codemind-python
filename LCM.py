1577c4c1bec9cd534a4d4247519899497dcc80e3
YasaswiniKoti <119840576+YASASWINIKOTI@users.noreply.github.com>
2023-05-19 17:08:13 +0530

    def compute_lcm(x, y):
       # choose the greater number
       if x > y:
           greater = x
       else:
           greater = y
    
       while(True):
           if((greater % x == 0) and (greater % y == 0)):
               lcm = greater
               break
           greater += 1
    
       return lcm
    a,b=map(int,input().split())
    print(compute_lcm(a,b))

---
+++ LCM.py
@@ -0,0 +1,17 @@
+def compute_lcm(x, y):
+
+   # choose the greater number
+   if x > y:
+       greater = x
+   else:
+       greater = y
+
+   while(True):
+       if((greater % x == 0) and (greater % y == 0)):
+           lcm = greater
+           break
+       greater += 1
+
+   return lcm
+a,b=map(int,input().split())
+print(compute_lcm(a,b))
