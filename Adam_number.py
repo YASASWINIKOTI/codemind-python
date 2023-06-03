a71fa530c50b612b6619536d840aec2a2b98d92a
YasaswiniKoti <119840576+YASASWINIKOTI@users.noreply.github.com>
2023-06-03 19:05:30 +0530

    n=int(input()) s1=n**2 s=0 sum2=0 while(n>0):     r=n%10     s=s*10+r     n=n//10     s2=s**2 while(s2>0):     rev=s2%10     sum2=sum2*10+rev     s2=s2//10 if(sum2==s1):     print("True") else:     print("False")

--- Adam_number.py
+++ Adam_number.py
@@ -2,11 +2,11 @@ n=int(input())
 s1=n**2
 s=0
 sum2=0
-while(n>0):
+while(n>0): 
     r=n%10
     s=s*10+r
     n=n//10
-s2=s**2
+    s2=s**2
 while(s2>0):
     rev=s2%10
     sum2=sum2*10+rev
@@ -15,4 +15,3 @@ if(sum2==s1):
     print("True")
 else:
     print("False")
-    
