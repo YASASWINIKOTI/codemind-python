4443e32417b5ef80c4d3dfa73a41de795a9db833
YasaswiniKoti <119840576+YASASWINIKOTI@users.noreply.github.com>
2023-06-03 19:12:40 +0530

    a=int(input()) for i in range(a):     n=int(input())     k=n     s=0     while(n!=0):         r=n%10         s=s*10+r         n=n//10     if(k==s):         print("True")     else:         print("False")

---
+++ Palindrome_Number.py
@@ -0,0 +1,13 @@
+a=int(input())
+for i in range(a):
+    n=int(input())
+    k=n
+    s=0
+    while(n!=0):
+        r=n%10
+        s=s*10+r
+        n=n//10
+    if(k==s):
+        print("True")
+    else:
+        print("False")
